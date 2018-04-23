3b995f07079e52e42d6a055af2146ddbd371c851
Tiago Zimmermann <tiagozimmermann88@gmail.com>
2018-04-23 14:27:04 -0300

    updated schema with log_users default

--- install/database/schema.sql
+++ install/database/schema.sql
@@ -822,6 +822,21 @@ CREATE TABLE IF NOT EXISTS `itc_consumers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Table structure for table `logs_users`
+--
+CREATE TABLE IF NOT EXISTS `logs_users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL,
+  `ip` varchar(15) NOT NULL,
+  `user` varchar(30) NOT NULL,
+  `action` varchar(30) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `table` varchar(30) NOT NULL,
+  `registerid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
 -- Indexing table `cdr`
 --
 CREATE INDEX cdr_clid ON cdr (clid(30));
